b62a629801e1fca3c34764b7b9f290c2706f1d90
qiquzuo <zzhqiqu@foxmail.comm>
2020-12-12 18:09:46 +0800

    tinajia

---
+++ src/main/java/com/wdjr/springboot/bean/a.java
@@ -0,0 +1,11 @@
+package src.main.java.com.wdjr.springboot.bean;
+
+public class a {
+   int a=0;
+   String sdfsfsdfsd;
+}
+
+
+
+//C:\Users\qiqu\IdeaProjects\shen\src\main\java\com\wdjr\springboot\bean\a.java
+
---
+++ src/main/java/com/wdjr/springboot/bean/aasdasd.java
@@ -0,0 +1,4 @@
+package src.main.java.com.wdjr.springboot.bean;
+
+public class aasdasd {
+}
